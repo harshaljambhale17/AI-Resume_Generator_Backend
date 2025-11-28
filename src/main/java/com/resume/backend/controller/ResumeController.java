1d1176697778389e25fca0744776f1d461a27e2e
harshaljambhale17 <harshaljambhale17@gmail.com>
2025-11-28 15:23:11 +0530

    properties update

--- src/main/java/com/resume/backend/controller/ResumeController.java
+++ src/main/java/com/resume/backend/controller/ResumeController.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@CrossOrigin("https://ai-resume-generator-frontend-omega.vercel.app")
+@CrossOrigin(origins = {"https://ai-resume-generator-frontend-omega.vercel.app", "http://localhost:5173"})
 @RequestMapping("/api/v1/resume")
 public class ResumeController {
 
